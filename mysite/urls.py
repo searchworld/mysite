6d2ff726a123208ff491c14b9859017297d25ab4
zhangzb <zhangzb2007@gmail.com>
2015-02-09 14:01:54 +0800

    test

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,11 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
-<<<<<<< HEAD
     # url(r'^blog/', include('blog.urls')),
-=======
-    url(r'^blog/', include('blog.urls')),
->>>>>>> test
 
     url(r'^admin/', include(admin.site.urls)),
 )
